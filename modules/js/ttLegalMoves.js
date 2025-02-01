be8a49759ee1e04a872ce763e5881ae8fa7169e6
David Felcan <dfelcan@gmail.com>
2025-02-01 16:24:15 -0500

    Did not commit ttLegalMoves.js

---
+++ modules/js/ttLegalMoves.js
@@ -0,0 +1,13 @@
+//* Returns all legal moves 
+
+define([
+    "dojo", "dojo/_base/declare"
+], function( dojo, declare )
+
+{
+    return declare("bgagame.ttLegalMoves", null, 
+    { 
+
+    }
+    );
+});
